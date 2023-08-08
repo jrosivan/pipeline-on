58b03640d1c1dc229040331e25f6ed4bf7bee4b2
Jorge Rosivan R. Batista <42048953+jrosivan@users.noreply.github.com>
2023-08-08 20:17:58 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to MBA - 2023 - 1SCJR"</h1>
+          <h1 className="App-title">Welcome to MBA - 2023 - 1SCJR - [ROSIVAN]"</h1>
         </header>
       </div>
     );
